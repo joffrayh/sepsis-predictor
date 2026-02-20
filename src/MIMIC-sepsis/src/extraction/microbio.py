829a935ff6a21af44f7eff85335e7ea20785f19d
joffrayh <joffrayh@icloud.com>
2026-02-20 14:08:56 +0000

    updated to stream data directly to csv

--- src/MIMIC-sepsis/src/extraction/microbio.py
+++ src/MIMIC-sepsis/src/extraction/microbio.py
@@ -1,36 +1,43 @@
-"""
-MIMIC-IV Sepsis Cohort Extraction.
-
-This file is sourced and modified from: https://github.com/matthieukomorowski/AI_Clinician
-"""
-
 import argparse
 import os
-
-import pandas as pd
 import psycopg2 as pg
 
-
+# parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("-u", "--username",  help="Username used to access the MIMIC Database", type=str)
-parser.add_argument("-p", "--password",  help="User's password for MIMIC Database", type=str)
+parser.add_argument("-u", "--username", help="MIMIC Database Username", type=str, required=True)
+parser.add_argument("-p", "--password", help="MIMIC Database Password", type=str, default="")
 pargs = parser.parse_args()
 
 # Initializing database connection
-conn = pg.connect("dbname='mimiciv' user={0} host='localhost' options='--search_path=mimimciv' password={1}".format(pargs.username,pargs.password))
+conn = pg.connect("dbname='mimiciv' user={0} host='localhost' options='--search_path=mimiciv' password={1}".format(pargs.username,pargs.password))
 
-# Path for processed data storage
-exportdir = os.path.join(os.getcwd(),'processed_files')
-
+# create output dir
+exportdir = os.path.join(os.getcwd(), 'processed_files')
 if not os.path.exists(exportdir):
     os.makedirs(exportdir)
 
+output_file = os.path.join(exportdir,'microbio.csv')
+
+
 # 2. microbio (Microbiologyevents)
 # extract(epoch from charttime) : The number of seconds since 1970-01-01 00:00:00 UTC
-query = """
-select subject_id, hadm_id, extract(epoch from charttime) as charttime, extract(epoch from chartdate) as chartdate 
-from mimiciv_hosp.microbiologyevents
-"""
 
-d = pd.read_sql_query(query,conn)
-d.to_csv(os.path.join(exportdir, 'microbio.csv'),index=False,sep='|')
+print("Extracting microbiology events data...")
+
+with conn.cursor() as cur:
+    with open(output_file, 'w') as f:
+        cur.copy_expert(
+            """
+            COPY (
+                select subject_id,
+                    hadm_id, 
+                    extract(epoch from charttime) as charttime, 
+                    extract(epoch from chartdate) as chartdate 
+                from mimiciv_hosp.microbiologyevents
+            )
+            TO STDOUT WITH CSV HEADER DELIMITER '|'
+            """,
+            f
+        )
+
+print(f"Success! Data saved to: {output_file}")
